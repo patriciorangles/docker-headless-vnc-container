03d77b5732e9cc9caa1b96be00e28d386feb3301
Patricio Rangles <patricio.rangles@trescloud.com>
2023-02-24 11:41:25 -0500

    [IMP][VSC]: arquitectura

--- src/common/install/VSC_install.sh
+++ src/common/install/VSC_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #Instalacion de Visual Studio Code
 
+ARCH=$(dpkg --print-architecture)
+
 apt-get update && apt-get install -y apt-transport-https curl gpg
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg \
 && install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/ \
-&& echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+&& echo "deb [arch=$ARCH] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 apt-get update && apt-get install -y code libx11-xcb-dev libasound2
 

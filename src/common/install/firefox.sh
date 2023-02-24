355e9286d5c8b35c6a982251bcf3b63eb89ccd67
Patricio Rangles <patricio.rangles@trescloud.com>
2023-02-24 12:43:04 -0500

    [FIX][VSC,firefox]: corregido instalacion

--- src/common/install/firefox.sh
+++ src/common/install/firefox.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+# agregado manejo de arm64
 set -e
 
-VERSION="102.3.0esr"
+VERSION="110.0"
 echo "Install Firefox $VERSION"
+
+arch_system=$(dpkg --print-architecture)
 
 function disableUpdate(){
     ff_def="$1/browser/defaults/profile"
@@ -20,23 +23,28 @@ EOF_FF
 }
 
 function instFF() {
-    if [ ! "${1:0:1}" == "" ]; then
-        FF_VERS=$1
-        if [ ! "${2:0:1}" == "" ]; then
-            FF_INST=$2
-            echo "download Firefox $FF_VERS and install it to '$FF_INST'."
-            mkdir -p "$FF_INST"
-            FF_URL=http://releases.mozilla.org/pub/firefox/releases/$FF_VERS/linux-x86_64/en-US/firefox-$FF_VERS.tar.bz2
-            echo "FF_URL: $FF_URL"
-            wget -qO- $FF_URL | tar xvj --strip 1 -C $FF_INST/
-            ln -s "$FF_INST/firefox" /usr/bin/firefox
-            # Enable Update
-            #disableUpdate $FF_INST
-            exit $?
+    if [ "$arch_system" == "amd64" ]; then
+        if [ ! "${1:0:1}" == "" ]; then
+            FF_VERS=$1
+            if [ ! "${2:0:1}" == "" ]; then
+                FF_INST=$2
+                echo "download Firefox $FF_VERS and install it to '$FF_INST'."
+                mkdir -p "$FF_INST"
+                FF_URL=http://releases.mozilla.org/pub/firefox/releases/$FF_VERS/linux-x86_64/en-US/firefox-$FF_VERS.tar.bz2
+                echo "FF_URL: $FF_URL"
+                wget -qO- $FF_URL | tar xvj --strip 1 -C $FF_INST/
+                ln -s "$FF_INST/firefox" /usr/bin/firefox
+                # Enable Update
+                #disableUpdate $FF_INST
+                exit $?
+            fi
         fi
+        echo "function parameter are not set correctly please call it like 'instFF [version] [install path]'"
+        exit -1
+    elif [ "$arch_system" == "arm64" ]; then
+        echo "No existe firefox en ARM64, se usa firefox-esr"
+        apt-get update && apt-get install -y firefox-esr
     fi
-    echo "function parameter are not set correctly please call it like 'instFF [version] [install path]'"
-    exit -1
 }
 
 instFF "$VERSION" '/usr/lib/firefox'

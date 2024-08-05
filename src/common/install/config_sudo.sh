dc8e4fa3a1221f44c01701901cc7a3637f6feb11
Patricio Rangles <patricio.rangles@trescloud.com>
2024-08-05 13:30:56 -0500

    [FIX] problemas con permisos

--- src/common/install/config_sudo.sh
+++ src/common/install/config_sudo.sh
@@ -2,15 +2,25 @@
 ### every exit != 0 fails the script
 set -e
 
-USER_NAME=$1
+USER_ID=$1
 HOME=$2
 
-echo "Configure sudo for user: $USER_NAME, Home directory: $HOME"
+echo "Configure sudo for user with ID: $USER_ID, Home directory: $HOME"
+
+# Get the username associated with the provided user ID
+USER_NAME=$(getent passwd "$USER_ID" | cut -d: -f1)
+
+if [ -z "$USER_NAME" ]; then
+    echo "No user found with ID: $USER_ID"
+    exit 1
+fi
+
+echo "User name found: $USER_NAME"
 
 # create user
-adduser --home $HOME --disabled-password --gecos '' $USER_NAME
+adduser --home "$HOME" --disabled-password --gecos '' "$USER_NAME"
 # add user to group sudo
-adduser $USER_NAME sudo
-# add permisions to sudoers file
+adduser "$USER_NAME" sudo
+# add permissions to sudoers file
 echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 
